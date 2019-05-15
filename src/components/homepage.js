790600e9cb09212f30d41d10296c4bf1f1c1f632
Joshua Loran <joshua.loran@gmail.com>
2019-05-15 14:25:02 -0700

    homepage addition

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import Banner from '../images/home_control_banner.jpg'
 
+
 export default class Homepage extends Component {
 
   name(){
@@ -21,7 +22,7 @@ export default class Homepage extends Component {
     return (
 
       <div>
-        <img src={Banner} alt='banner here' />
+          <img src={Banner} alt='banner here' />
       <h2> Welcome, {this.name()}!</h2>
       </div>
     )
